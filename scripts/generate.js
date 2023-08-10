e36cc32af84157ee59933cfe24d8225625065c6e
Kiko Beats <josefrancisco.verdu@gmail.com>
2023-08-10 17:43:04 +0200

    chore: better script

--- scripts/generate.js
+++ scripts/generate.js
@@ -4,31 +4,41 @@ const crawlers = require('crawler-user-agents')
 const { writeFile } = require('fs/promises')
 const { load } = require('cheerio')
 const pFilter = require('p-filter')
+const pEvery = require('p-every')
 
-const EMOJI = { true: '✅', false: '❌' }
+const CHECK = { true: '✅', false: '❌' }
 const MAX_CONCURRENCY = 10
+const REQ_TIMEOUT = 10000
 
-const candidates = crawlers.map(crawler => crawler.instances[0]).filter(Boolean)
+const candidates = [...new Set(crawlers.flatMap(crawler => crawler.instances))]
 
-const verify = async (userAgent, index) => {
+const URLS = [
+  'https://twitter.com/Kikobeats/status/1687837848802578432'
+  // 'https://www.tesla.com/nl_NL/m3/order/LRW3228_606edc0abaa8666f9e88d89a1b30e988?titleStatus=new&redirect=no#overview'
+]
+
+const verifyUrl = userAgent => async url => {
   try {
-    const res = await fetch(
-      'https://twitter.com/Kikobeats/status/1687837848802578432',
-      {
-        headers: { 'user-agent': userAgent, redirect: 'manual' }
-      }
-    )
+    const controller = new AbortController()
+    setTimeout(() => controller.abort(), REQ_TIMEOUT)
+    const res = await fetch(url, {
+      signal: controller.signal,
+      headers: { 'user-agent': userAgent, redirect: 'manual' }
+    })
     const html = await res.text()
     const $ = load(html)
     const imageUrl = $('meta[property="og:image"]').attr('content')
-    const isValid = !!imageUrl
-    console.log(`${index}/${candidates.length} ${userAgent} ${EMOJI[isValid]}`)
-    return isValid
+    return !!imageUrl
   } catch (_) {
-    console.log(`${index}/${candidates.length} ${userAgent} ${EMOJI.false}`)
     return false
   }
 }
+
+const verify = async (userAgent, index) =>
+  pEvery(URLS, verifyUrl(userAgent)).then(result => {
+    console.log(`${CHECK[result]} ${index}/${candidates.length} ${userAgent}`)
+    return result
+  })
 
 Promise.resolve()
   .then(() => pFilter(candidates, verify, { concurrency: MAX_CONCURRENCY }))
